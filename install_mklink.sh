7c83e1452dc48c326a3c81db7791b113f6bc5e09
Yuriy VG <yuravg@gmail.com>
2020-11-19 14:48:07 +0300

    Add installation script for Linux
    this script make Linux symbolic links.

---
+++ install_mklink.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Installation script for Linux
+#
+# This script creates Linux symbolic links in the $HOME/bin directory pointing to local files
+#
+
+echo "Make links:"
+
+PATH2SCRIPT="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+mklink () {
+    fname="$1"
+    ln -v -s "$PATH2SCRIPT"/bin/"$fname" "$HOME/bin/$fname"
+}
+
+mklink git-clear-reflog
+mklink git-status-extra
